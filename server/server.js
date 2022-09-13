1b45e81faa41d028022d937c7049895b3a4c8526
donyateimouri <92165497+donyateimouri@users.noreply.github.com>
2022-09-13 11:59:16 +0200

    update server.js
    update server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
-import fetch from 'node-fetch'
+import  fs  from 'fs';
 import cors from 'cors';
 
 const app = express()
-const port = 3031
+const port = 4000
+const productsJSON = './data/products.json'
+
 //  get(route,callback function);
 app.get("/api", (req, res) => {
     res.send("Get Products Server")
@@ -14,68 +16,69 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // Routes
+// GET all products
+app.get("/api/home", async (req, res) => {
+    fs.readFile(productsJSON, 'utf8', (err, data) => {
+        const homeData = JSON.parse(data);
+        let laptops = homeData.laptops
+        let smartphones = homeData.smartphones
+        let sliders = homeData.sliders;
+        res.json({
+            laptops,
+            smartphones,
+            sliders
+        })
+    });
+})
 // Get a specific product with id
 app.get("/api/products/:id", async (req, res) => {
     const id = req.params.id
-    let product = await fetch(`https://dummyjson.com/products/${id}`)
-    let productJSON = await product.json();
-    res.json({
-        product : productJSON,
-    })
+    fs.readFile(productsJSON, 'utf8', (err, data) => {
+        const allData = JSON.parse(data);
+        let products = allData.laptops.concat(allData.smartphones);
+        const product = products.filter(product => product.id == id)[0];
+        res.json(product)
+    });
 })
+// Add a new product
 
-// Login
-app.post("/api/login", async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-
-    let result = await fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          username: username,
-          password: password,
+app.post("/api/products", async (req, res) => {
+    fs.readFile(productsJSON, 'utf8', (err, data) => {
+        let products = JSON.parse(data);
+        let product = getProduct(req.body);
+        products[req.body.category].push(product);
+        let newDataString = JSON.stringify(products) 
+        fs.writeFile(productsJSON, newDataString , (err) => {
+            res.json({
+                status : true,
+                product
+            });
         })
-      })
-      let login = await result.json();
-    res.json(login)
+    });
+
 })
-
-
-
-
-
-// GET all products
-app.get("/api/home", async (req, res) => {
-    let laptops = await fetch(`https://dummyjson.com/products/category/laptops`)
-    let laptopsJSON = await laptops.json();
-    let smartphones = await fetch(`https://dummyjson.com/products/category/Smartphones`)
-    let smartphonesJSON = await smartphones.json();
-    let sliders = [
-        '/assets/image/banner1.jpg',
-        '/assets/image/banner2.jpg',
-    ];
-
-    res.json({
-        laptops : laptopsJSON,
-        smartphones : smartphonesJSON,
-        sliders : sliders
-    })
-})
-// GET all categories
-app.get("/api/categories", async (req, res) => {
-    let response = await fetch(`https://dummyjson.com/products/categories`)
-    let result = await response.json();
-    res.json(result)
-})
-// Get products of a category 
-app.get("/api/products/categories/:category", async (req, res) => {
-    const category = req.params.category
-    let response = await fetch(`https://dummyjson.com/products/category/${category}`)
-    let result = await response.json();
-    res.json(result)
-})
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
+
+
+function getProduct(data){
+    return {
+        "id": 6,
+        "title": data.title ?? 'No Title',
+        "description": data.description ?? 'Not Description',
+        "price": data.price ?? 'Free',
+        "discountPercentage": 11.02,
+        "rating": 4.57,
+        "stock": data.stock ?? 0,
+        "brand": data.brand ?? 'No Brand',
+        "category": data.category,
+        "thumbnail": "https://dummyjson.com/image/i/products/6/thumbnail.png",
+        "images": [
+          "https://dummyjson.com/image/i/products/6/1.png",
+          "https://dummyjson.com/image/i/products/6/2.jpg",
+          "https://dummyjson.com/image/i/products/6/3.png",
+          "https://dummyjson.com/image/i/products/6/4.jpg"
+        ]
+      }
+}
